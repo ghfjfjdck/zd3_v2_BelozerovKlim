a1ebc38ebe7246f01d42b9389926bcf592e22a88
ghfjfjdck <157564400+ghfjfjdck@users.noreply.github.com>
2024-11-11 13:43:41 +0500

    Add files via upload

---
+++ ZD3/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZD3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Qp road = new Qp();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            road.Addobject(num1,num2,num3,num4);
+            MessageBox.Show("Добавлено");
+            road.Listrestart(listBox1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int d = listBox1.SelectedIndex;
+            if (d!=-1)
+            {
+
+                listBox1.Items.RemoveAt(d);
+
+
+            }else
+            {
+                MessageBox.Show("Выберите строчку");
+            }
+        }
+    }
+}
---
+++ ZD3/Q.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZD3
+{
+    class Q
+    {
+
+        public double Roadwidth;
+        public double dlina;
+        public double massa;
+        public double q;
+
+        public double Qmath(double w, double d, double m)
+        {
+            double result = w * d * (m / 1000);
+                q = result;
+            return result;
+        }
+
+      
+        
+
+
+    }
+}
